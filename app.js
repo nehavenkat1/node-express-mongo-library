dc9c3dc2c6a1050d971aa8a4057a2760aa9f7880
nehavenkat1 <nehavenky@yahoo.com>
2022-04-19 07:56:15 +0530

    chnages made for production

--- app.js
+++ app.js
@@ -16,13 +16,17 @@ var app = express();
 
 app.use(helmet())
 
+/*
 var url = 'mongodb+srv://neha:manavshri2324@cluster0.jafaw.mongodb.net/local_library?retryWrites=true&w=majority'
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
+*/
+
+var dev_db_url = 'mongodb+srv://neha:manavshri2324@cluster0.jafaw.mongodb.net/local_library?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true})
 
 var db = mongoose.connection
 db.on('error', console.error.bind(console, "MongoDB connection error."))
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
